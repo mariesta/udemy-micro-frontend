045eb1cb690dc85c7ce52c7d82a937de95131747
Marie Starck <marie.compsci14@gmail.com>
2023-10-09 16:30:43 -0400

    mount product module through a function

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex'
+import { mount } from 'products/ProductsIndex'
 import 'cart/CartShow'
+
+const el = document.querySelector('#my-products');
+mount(el);
 
 console.log('container!!!!');
